d35086c18852b6d476b4603bf56195a170625907
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-05-31 23:09:46 -0300

    fim da validação

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express")
 var app = express()
 var session = require("express-session")
 var flash = require("express-flash")
+var cookieParser = require("cookie-parser")
 
 app.set('view engine','ejs')
 
@@ -10,21 +11,42 @@ app.use(express.urlencoded({ extended: false }))
 // parse application/json
 app.use(express.json())
 
+app.use(cookieParser("hauhua"))
+
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+    cookie: { maxAge:60000 }
   }))
 
 app.use(flash())
 
 app.get("/",(req,res)=>{
-    res.render("index")
+
+    var emailError = req.flash("emailError") // esse error vai atras de uma flash session que tenha esse nome
+    var pontosError = req.flash("pontosError")
+    var nomeError = req.flash("nomeError")
+    var email = req.flash("email")
+    var nome = req.flash("nome")
+    var pontos = req.flash("pontos")
+
+    // se não usassemos o ternario seria assim
+    // if(emailError != undefined){
+    //     if(emailError.length == 0){
+    //         emailError = undefined
+    //     }
+    // }
+
+    emailError = (emailError == undefined || emailError.length == 0) ? undefined : emailError
+    email = (email == undefined || email.length == 0)? "": email
+    nome = (nome == undefined || nome.length > 4)?"": nome
+    pontos = (pontos == undefined || pontos.length > 20)?"":pontos
+    res.render("index",{emailError,pontosError,nomeError,email: email,nome: nome,pontos:pontos})
 })
 
 app.post("/form",(req,res)=> {
-    var {email,nome, pontos} = req.body
+    var {email,nome, pontos} = req.body // é usado essa variavel aqui para salvar o ultio e-mail que foi digitado
 
     var emailError
     var pontosError
@@ -44,8 +66,22 @@ app.post("/form",(req,res)=> {
         nomeError = "O nome não pode ser vazio"
 
     }
-    if(emailError !=)
-    res.send(email)
+    if(nome.length < 4){
+        nomeError = "O nome é mt pequeno"
+    }
+
+    if(emailError != undefined || pontosError != undefined || nomeError != undefined){
+        req.flash("emailError",emailError)
+        req.flash("pontosError",pontosError)
+        req.flash("nomeError",nomeError)
+        req.flash("email",email) // recebe a variavel var{emial,nome,pontos} = req.body
+        req.flash("nome",nome)
+        req.flash("pontos",pontos)
+        res.redirect("/")
+    }else{
+        res.send("SHOW DE BOLA ESSE FORM!")
+    }
+    
 })
 
 
